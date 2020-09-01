175baab0eae7236b4da764124d605a700c4fdf18
robi1467 <robi1467@morris.umn.edu>
2020-08-31 21:49:25 -0500

    Quality of life change to wrap_contents
    Threw in spaces to allow file to be more readable

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 #A Script to combine 3 files
+
 #Variables
 ToWrap=$1
 Specify=$2
 Result=$3
+
 #MAIN
 cat "$Specify"_header.html "$ToWrap" "$Specify"_footer.html > "$Result"
 #To my understanding cat is combbining the files in the order that they are placed in a specific file
